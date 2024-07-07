307db9cb0fec1f086afc2551ad0e8e2111473b53
Nirbhay <nirbhaysingh943012@gmail.com>
2024-07-07 20:58:44 +0530

    printName in Java

--- DSA/BASICS/Hello.java
+++ DSA/BASICS/Hello.java
@@ -1,5 +1,11 @@
 public class Hello {
+
+    public static void printName(String name) {
+        System.out.println(name);
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello World!!!");
+        // System.out.println("Hello World!!!");
+        printName("Nirbahy Singh");
     }
 }
