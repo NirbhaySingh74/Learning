59cda7c08ff68a7dd0e84942deea86ef36647baf
Nirbhay <nirbhaysingh943012@gmail.com>
2024-07-08 22:41:00 +0530

    First five pattern question from striver sheet

---
+++ DSA/PATTERN/pattern.java
@@ -0,0 +1,56 @@
+package DSA.PATTERN;
+
+public class pattern {
+    public static void first(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void second(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void three(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void four(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void five(int n) {
+        for (int i = n; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        first(5);
+        second(5);
+        three(5);
+        four(5);
+        five(5);
+    }
+}
