4871adc1ec6aba2221cb6381d5e2c0d772bc2f0f
Nirbhay <nirbhaysingh943012@gmail.com>
2024-07-09 21:00:13 +0530

    6th and 7th pattern question from striver sde sheet

--- DSA/PATTERN/pattern.java
+++ DSA/PATTERN/pattern.java
@@ -46,11 +46,40 @@ public class pattern {
         }
     }
 
+    public static void six(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= (n + 1) - i; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void seven(int n) {
+        for (int i = 1; i <= n; i++) {
+            // for space
+            for (int j = 1; j <= (n - i); j++) {
+                System.out.print(" ");
+            }
+            // for star
+            for (int k = 1; k <= (n - i); k++) {
+                System.out.print("*");
+            }
+            // for space
+            for (int j = 1; j <= (n - i); j++) {
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args) {
-        first(5);
-        second(5);
-        three(5);
-        four(5);
-        five(5);
+        // first(5);
+        // second(5);
+        // three(5);
+        // four(5);
+        // five(5);
+        // six(5);
+        seven(5);
     }
 }
