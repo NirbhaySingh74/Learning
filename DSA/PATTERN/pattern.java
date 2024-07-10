f378bbf9109366938c507aee50fdfaae49611d3b
Nirbhay <nirbhaysingh943012@gmail.com>
2024-07-11 00:31:06 +0530

    invert star pyramid done

--- DSA/PATTERN/pattern.java
+++ DSA/PATTERN/pattern.java
@@ -55,8 +55,26 @@ public class pattern {
         }
     }
 
-    public static void seven(int n) {
+    public static void starPyramid(int n) {
         for (int i = 1; i <= n; i++) {
+            // for space
+            for (int j = 1; j <= (n - i); j++) {
+                System.out.print(" ");
+            }
+            // for star
+            for (int k = 1; k <= (2 * i) - 1; k++) {
+                System.out.print("*");
+            }
+            // for space
+            for (int j = 1; j <= (n - i); j++) {
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void invertStarPyramid(int n) {
+        for (int i = n; i >= 1; i--) {
             // for space
             for (int j = 1; j <= (n - i); j++) {
                 System.out.print(" ");
@@ -80,6 +98,7 @@ public class pattern {
         // four(5);
         // five(5);
         // six(5);
-        seven(5);
+        // starPyramid(5);
+        invertStarPyramid(5);
     }
 }
