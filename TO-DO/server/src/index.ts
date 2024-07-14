28776860a04667e8fea28ffb13f9c7ff5755c757
Nirbhay <nirbhaysingh943012@gmail.com>
2024-07-14 17:53:14 +0530

    backend confiuration done!

--- TO-DO/server/src/index.ts
+++ TO-DO/server/src/index.ts
@@ -1,2 +1,8 @@
-console.log(2 + 3);
-console.log(4);
+import express, { Application } from "express";
+
+const app: Application = express();
+const PORT: number = parseInt(process.env.PORT as string, 10) || 7000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
