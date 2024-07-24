8e2bcb1057f6ee27f11a594c8762b298ac4ad475
Nirbhay <nirbhaysingh943012@gmail.com>
2024-07-24 19:42:36 +0530

    countDigit

---
+++ DSA/BASICMATH/countDigits.java
@@ -0,0 +1,13 @@
+package DSA.BASICMATH;
+
+public class countDigits {
+    public static void main(String[] args) {
+        int num = 154673745;
+        int count = 0;
+        while (num > 0) {
+            num = num / 10;
+            count++;
+        }
+        System.out.println(count);
+    }
+}
