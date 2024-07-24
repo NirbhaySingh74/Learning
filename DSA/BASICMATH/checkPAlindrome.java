909d3f2ba170ba1f54771316fff547ed9ff77b1e
Nirbhay singh <105772373+NirbhaySingh74@users.noreply.github.com>
2024-07-25 00:39:58 +0530

    checkPalindrome

---
+++ DSA/BASICMATH/checkPAlindrome.java
@@ -0,0 +1,27 @@
+package DSA.BASICMATH;
+
+public class checkPalindrome {
+
+    public static boolean isPalindrome(int num) {
+        int originalNum = num;
+        int reversedNum = 0;
+
+        while (num > 0) {
+            int rem = num % 10;
+            reversedNum = reversedNum * 10 + rem;
+            num = num / 10;
+        }
+
+        return originalNum == reversedNum;
+    }
+
+    public static void main(String[] args) {
+        int num = 121;
+        if (isPalindrome(num)) {
+            System.out.println(num + " is a palindrome.");
+        } else {
+            System.out.println(num + " is not a palindrome.");
+        }
+    }
+
+}
