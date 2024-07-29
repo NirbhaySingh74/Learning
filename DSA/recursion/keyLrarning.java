94f51b8822bc52691329e1d2376067fbbe9956e4
Nirbhay singh <105772373+NirbhaySingh74@users.noreply.github.com>
2024-07-29 22:13:30 +0530

    keyLearning

---
+++ DSA/recursion/keyLrarning.java
@@ -0,0 +1,8 @@
+package DSA.recursion;
+
+public class keyLrarning {
+
+    public static void main(String[] args) {
+        System.out.println("when you try to solve one problem during solving this problem you think i will relax after solving it but it not happen you will be ended up getting bigger problem so never think you will not have any problem. it's part of our life we can't stop it");
+    }
+}
