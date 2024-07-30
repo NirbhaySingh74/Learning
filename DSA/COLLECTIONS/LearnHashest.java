379cb0ce41ea8210b73e9d58380e063a1e6e08c4
Nirbhay <nirbhaysingh943012@gmail.com>
2024-07-30 22:39:57 +0530

    iterate on hashset

--- DSA/COLLECTIONS/LearnHashest.java
+++ DSA/COLLECTIONS/LearnHashest.java
@@ -1,4 +1,5 @@
 import java.util.HashSet;
+import java.util.Iterator;
 
 public class LearnHashest {
     public static void main(String[] args) {
@@ -6,9 +7,17 @@ public class LearnHashest {
         set.add("Nirbhay");
         set.add("Kunal");
         set.add("Nirbhay");
+        set.add("Deepak");
         // print all element in hashset
         System.out.println(set);
         // is contains element
         System.out.println(set.contains("Nirbhay"));
+
+        // iterate on hasheset
+        Iterator it = set.iterator();
+
+        while (it.hasNext()) {
+            System.out.println(it.next());
+        }
     }
 }
