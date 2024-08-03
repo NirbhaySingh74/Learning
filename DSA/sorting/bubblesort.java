f58994492a5f2ee18274587aebf8534231dd3f7b
Nirbhay <nirbhaysingh943012@gmail.com>
2024-08-03 22:42:39 +0530

    bubblesort

---
+++ DSA/sorting/bubblesort.java
@@ -0,0 +1,23 @@
+package DSA.sorting;
+
+import java.util.Arrays;
+
+public class bubblesort {
+    public static int[] bubble_sort(int arr[], int n) {
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = 0; j <= i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 13, 46, 24, 52, 20, 9 };
+        System.out.println(Arrays.toString(bubble_sort(arr, arr.length)));
+    }
+}
