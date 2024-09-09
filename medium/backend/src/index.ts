924ea32e0cee8fc95e6291bf64acfab381d2db3f
Nirbhay Singh <nirbhaysingh943012@gmail.com>
2024-09-09 18:57:59 +0530

    user signup route created

--- medium/backend/src/index.ts
+++ medium/backend/src/index.ts
@@ -1,9 +1,29 @@
 import { Hono } from "hono";
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
+import { jwt, sign } from "hono/jwt";
+const app = new Hono<{
+  Bindings: {
+    DATABASE_URL: string;
+    JWT_SECRET: string;
+  };
+}>();
 
-const app = new Hono();
-
-app.post("/api/v1/signup", (c) => {
-  return c.text("signup route");
+app.post("/api/v1/signup", async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+  const body = await c.req.json();
+  const user = await prisma.user.create({
+    data: {
+      email: body.email,
+      password: body.password,
+    },
+  });
+  const token = sign({ id: user.id }, c.env.JWT_SECRET);
+  return c.json({
+    jwt: token,
+  });
 });
 
 app.post("/api/v1/signin", (c) => {
